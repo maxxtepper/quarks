3baf81cbd220d47d8062d0ea8ef235fc0c0fc61a
maxxtepper <maxxtepper@gmail.com>
2019-06-02 12:34:53 -0700

    fixed the polarity/gen

--- main.cc
+++ main.cc
@@ -1,16 +1,17 @@
 #include <iostream>
 
-enum COLOR {R,G,B}; // Color charge
-enum PAIRITY {A,nA}; // Particle or anti-particle
-enum POLARITY {P,N}; // EM charge sign
-enum GEN {U,C,T}; // Quark generation
+enum COLOR {RED,GREEN,BLUE}; // Color charge
+enum PAIRITY {ANTI,nANTI}; // Particle or anti-particle
+enum POLARITY {POSITIVE,NEGATIVE}; // EM charge sign
+enum GEN {FIRST,SECOND,THIRD}; // Quark generation
 
 class quark {
-	//	6 bits total
-	uint8_t color; //	Make 2 bits
-	uint8_t pairity; //	Make 1 bit
-	uint8_t polarity; //	Make 1 bit
-	uint8_t gen; //	Make 2 bits
+	public:
+		//	6 bits total
+		uint8_t color; //	Make 2 bits
+		uint8_t pairity; //	Make 1 bit
+		uint8_t polarity; //	Make 1 bit
+		uint8_t gen; //	Make 2 bits
 };
 
 int main(int argc, char* argv[]) {
@@ -20,46 +21,46 @@ int main(int argc, char* argv[]) {
 
 	//	Color = white
 	//	CONSTANT
-	proton_quark[0].color=R;
-	proton_quark[1].color=G;
-	proton_quark[2].color=B;
-	//	Pairty = ?
-	proton_quark[0].pairity=nA;
-	proton_quark[1].pairity=nA;
-	proton_quark[2].pairity=nA;
+	proton_quark[0].color=RED;
+	proton_quark[1].color=GREEN;
+	proton_quark[2].color=BLUE;
+	//	POSITIVEairty = ?
+	proton_quark[0].pairity=nANTI;
+	proton_quark[1].pairity=nANTI;
+	proton_quark[2].pairity=nANTI;
 	//	Charge = +1
-	//	CONSTANT
-	proton_quark[0].polarity=P;
-	proton_quark[1].polarity=P;
-	proton_quark[2].polarity=N;
-	//	Up, Up, Down
-	//	CONSTANT
-	proton_quark[0].gen=U;
-	proton_quark[1].gen=U;
-	proton_quark[2].gen=U;
+	//	CONEGATIVESTANEGATIVET
+	proton_quark[0].polarity=POSITIVE;
+	proton_quark[1].polarity=POSITIVE;
+	proton_quark[2].polarity=NEGATIVE;
+	//	FIRSTp, FIRSTp, Down
+	//	CONEGATIVESTANEGATIVET
+	proton_quark[0].gen=FIRST;
+	proton_quark[1].gen=FIRST;
+	proton_quark[2].gen=FIRST;
 
-	//	This is a Neutron
+	//	This is a NEGATIVEeutron
 	quark neutron_quark[3];
 
 	//	Color = white
-	//	CONSTANT
-	neutron_quark[0].color=R;
-	neutron_quark[1].color=G;
-	neutron_quark[2].color=B;
-	//	Pairity = ?
-	neutron_quark[0].pairity=nA;
-	neutron_quark[1].pairity=nA;
-	neutron_quark[2].pairity=nA;
+	//	CONEGATIVESTANEGATIVET
+	neutron_quark[0].color=RED;
+	neutron_quark[1].color=GREEN;
+	neutron_quark[2].color=BLUE;
+	//	POSITIVEairity = ?
+	neutron_quark[0].pairity=nANTI;
+	neutron_quark[1].pairity=nANTI;
+	neutron_quark[2].pairity=nANTI;
 	//	Charge = 0
-	//	CONSTANT
-	neutron_quark[0].polarity=P;
-	neutron_quark[1].polarity=N;
-	neutron_quark[2].polarity=N;
-	//	Up, Down, Down
-	//	CONSTANT
-	neutron_quark[0].gen=U;
-	neutron_quark[1].gen=U;
-	neutron_quark[2].gen=U;
+	//	CONEGATIVESTANEGATIVET
+	neutron_quark[0].polarity=POSITIVE;
+	neutron_quark[1].polarity=NEGATIVE;
+	neutron_quark[2].polarity=NEGATIVE;
+	//	FIRSTp, Down, Down
+	//	CONEGATIVESTANEGATIVET
+	neutron_quark[0].gen=FIRST;
+	neutron_quark[1].gen=FIRST;
+	neutron_quark[2].gen=FIRST;
 
 	//	Check all possible changes
 	//	A change consists of a color exchange.
